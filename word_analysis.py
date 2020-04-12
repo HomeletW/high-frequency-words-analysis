472d0dd62984205fc8ea7e37d5b0ed92cfcd4b82
Homelet <homeletwei@gmail.com>
2020-04-12 17:12:23 -0400

    new statistics model

--- word_analysis.py
+++ word_analysis.py
@@ -1,11 +1,15 @@
 # coding=utf-8
 import sys
-from typing import Dict, List, Tuple, Union
+from typing import Type
 
+# 引用必要库
 import jieba.analyse
 import matplotlib.font_manager as mfm
 import matplotlib.pyplot as plt
 import pandas as pd
+import textract
+
+from word_statistics import *
 
 sys.path.append("./")
 
@@ -14,20 +18,22 @@ font_path = "./SourceHanSerifSC-Light.otf"
 prop = mfm.FontProperties(fname=font_path)
 shapes = ["v", "s", "h", "o", "*", "p", "P", "H", "+", "x", "X", "d", "D"]
 
-# 装载用户自定义词典
-# 测试：装载用户词典效果并不理想
-user_dict = "./user_dict"
-# jieba.analyse.set_idf_path(user_dict)
+# 建议关键词路径
+suggestion_path = "./suggestion_word.txt"
 
-# 可用的分析器
+# 装载用户自定义字典
+jieba.load_userdict(suggestion_path)
+
+# 可用的提取器
 TF_IDF = jieba.analyse.extract_tags
 TEXT_RANK = jieba.analyse.textrank
 
 
 def extract(sentence: str,
             suggestion_word: List[str],
-            threshold: int,
-            analyzer=TEXT_RANK) -> Dict[str, int]:
+            whitelist_word: List[str],
+            num_wanted: int,
+            extractor=TEXT_RANK) -> Dict[str, int]:
     """
     使用 jieba（中文分词）库，对文章进行关键词提取，使用 TextRank 或者 TF-IDF 算法。
 
@@ -37,22 +43,22 @@ def extract(sentence: str,
         论文：http://web.eecs.umich.edu/~mihalcea/papers/mihalcea.emnlp04.pdf
         基本思想:
             -将待抽取关键词的文本进行分词
-
             -以固定窗口大小(默认为5，通过span属性调整)，词之间的共现关系，构建图
             -计算图中节点的PageRank，注意是无向带权图
 
     参数列表如下：
     :param sentence: 文章本身
-    :param suggestion_word: 想要一定出现的关键词列表
-    :param threshold: 想要的关键词的个数 （抓取 10倍该值的关键词）
-    :param analyzer: 用做分析器的算法，应该为（TF_IDF 或者 TEXT_RANK)
+    :param suggestion_word: 想要一定被考虑的关键词列表
+    :param whitelist_word: 想要一定出现的关键词列表
+    :param num_wanted: 想要的关键词的个数 （抓取 10倍该值的关键词）
+    :param extractor: 用做提取器的算法，应该为（TF_IDF 或者 TEXT_RANK)
 
     返回参数如下：
-    :return: 返回一个字典，key为关键词，value关键词出现在文章里出现的次数
+    :return: 返回一个字典，键为关键词，值为关键词出现在文章里出现的次数
     """
     # use jieba.analyse (tf-idf) or (TextRank)
-    seg_list = analyzer(
-        sentence=sentence, topK=threshold * 10, withWeight=False, allowPOS=(
+    seg_list = extractor(
+        sentence=sentence, topK=num_wanted * 10, withWeight=False, allowPOS=(
             "n",  # 普通名词
             "nr", "PER",  # 人名
             "nz",  # 专有名词
@@ -62,77 +68,82 @@ def extract(sentence: str,
             "nw",  # 作品名
         )
     )
+    # use jieba.cut
+    segments = jieba.lcut(sentence=sentence)
     # summaries
-    summary = {seg: sentence.count(seg) for seg in seg_list}
-    summary.update({word: sentence.count(word) for word in suggestion_word if
+    summary = {seg: segments.count(seg) for seg in seg_list}
+    # add suggestion word and whitelist word
+    summary.update({word: segments.count(word) for word in suggestion_word if
+                    word not in summary})
+    summary.update({word: segments.count(word) for word in whitelist_word if
                     word not in summary})
     return summary
 
 
 def read_file(path: str) -> str:
-    """
-    打开一个文件并读取其全部资源
-    """
+    """打开一个文件并读取其全部资源"""
     with open(path, "r", encoding="UTF-8") as f:
         return f.read()
 
 
-def analyse(years: List[Dict[str, int]],
-            suggestion_word: List[str],
-            threshold: int) -> Dict[str, Union[List[int], List[str]]]:
-    """
-    Analyse the given list and conclude the most important portion of data
-    """
-    # construct a frequency count to find the highest frequency
-    frequency_count = {}
-    for year in years:
-        for word, count in year.items():
-            frequency_count[word] = frequency_count.get(word, 0) + count
-    # find the average
-    for word, count in frequency_count.items():
-        frequency_count[word] = count / len(years)
-    # find the top ones
-    top = sorted(list(frequency_count.items()),
-                 key=lambda a: a[1],
-                 reverse=True)[:threshold - len(suggestion_word)]
-    # get the word we want i.e the ones from suggestion_word or
-    # the top frequency
-    ret_words = suggestion_word + [word for word, _ in top]
-    return {
-        word: [year.get(word, 0) for year in years]
-        for word in ret_words
-    }
+def read_word_file(path: str) -> str:
+    """打开一个 .doc .docx 文件并读取其全部资源"""
+    return textract.process(path, encoding="UTF-8")
+
+
+def load_suggestion_word(path: str) -> List[str]:
+    """装载用户自定建议词典"""
+    ls = read_file(path).split("\n")
+    ls.remove("")
+    return ls
 
 
 def draw(input_files: List[Tuple[str, str]],
          suggestion_word: List[str],
-         threshold: int,
+         whitelist_word: List[str],
+         blacklist_word: List[str],
+         num_wanted: int,
          size: Tuple[int, int],
-         analyzer=TEXT_RANK) -> None:
+         extractor=TEXT_RANK,
+         statistics_analyzer: Type[StatisticalAnalyzer] = FrequencyAnalyzer,
+         *args, **kwargs) -> None:
     """
     展示一个图像来展示在数据中的高频词汇
 
     参数列表如下：
     :param input_files: 一个列表含有 <分析文件的路径> 与 <x轴的标签>
-    :param suggestion_word: 想要一定出现的关键词列表
-    :param threshold: 想要的关键词的个数
-    :param size: 生成的图像大小
-    :param analyzer: 用做分析器的算法，应该为（TF_IDF 或者 TEXT_RANK)
+    :param suggestion_word: 想要一定被考虑的关键词列表
+    :param whitelist_word: 想要一定出现的关键词列表
+    :param blacklist_word: 想要一定不出现的关键词列表
+    :param num_wanted: 想要的关键词的个数
+    :param size: 生成的图像大小,生产100倍像素的图像（例如：（12，6）生成图像为 1200px * 600px)
+    :param extractor: 用做提取器的算法，应该为（TF_IDF 或者 TEXT_RANK)
+    :param statistics_analyzer: 用做分析器的统计分析器，应为 StatisticalAnalyzer 的子类
     """
     header = [i[1] for i in input_files]
     input_path = [i[0] for i in input_files]
     # create a list
     years = [
-        extract(read_file(path), suggestion_word, threshold, analyzer)
-        for path in input_path
+        extract(
+            read_file(path), suggestion_word, whitelist_word, num_wanted,
+            extractor
+        ) for path in input_path
     ]
-    ranked_words = analyse(years, suggestion_word, threshold)
+    # create the statistical analyzer
+    analyzer = statistics_analyzer.const(
+        years, whitelist_word, blacklist_word
+    )
+    # analyze the important data
+    labeled_words = analyzer.analyse(*args, **kwargs)
+    ranked_words = {word: data for word, data, item in labeled_words if
+                    item["label"] == TREND_FLAG_STABLE}
     ranked_words.update({"x": header})
     # create data frame
     data = pd.DataFrame(ranked_words)
+    # draw
     plt.figure(1, figsize=size, dpi=100)
     ax = plt.subplot(111)
-    for word, s in zip(ranked_words, shapes):
+    for word, s in zip(ranked_words, shapes * 10):
         if word != "x":
             plt.plot("x",
                      word,
@@ -154,24 +165,29 @@ def draw(input_files: List[Tuple[str, str]],
                mode="expand",
                borderaxespad=0.,
                prop=prop)
+    # save the graph to the default location
+    # show the graph
     plt.show()
 
 
 def main():
+    prefix_path = "/example"
     input_files = [
-        ("./政府工作报告-2016-3-17.txt", "2016大"),
-        ("./政府工作报告-2017-3-5.txt", "2017大"),
-        ("./政府工作报告-2018-3-5.txt", "2018大"),
-        ("./政府工作报告-2019-3-5.txt", "2019大"),
+        (f".{prefix_path}/政府工作报告-2016-3-17.txt", "2016大"),
+        (f".{prefix_path}/政府工作报告-2017-3-5.txt", "2017大"),
+        (f".{prefix_path}/政府工作报告-2018-3-5.txt", "2018大"),
+        (f".{prefix_path}/政府工作报告-2019-3-5.txt", "2019大"),
     ]
     draw(
         input_files=input_files,
-        suggestion_word=[
-            "政府"
-        ],
-        threshold=10,
+        suggestion_word=load_suggestion_word(suggestion_path),
+        whitelist_word=[],
+        blacklist_word=[],
+        num_wanted=7,
         size=(12, 6),
-        analyzer=TEXT_RANK
+        extractor=TF_IDF,
+        statistics_analyzer=TrendAnalyzer,
+        # threshold=3,
     )
 
 
---
+++ word_statistics.py
@@ -0,0 +1,270 @@
+"""
+统计模块，用于总结重要高频词汇
+"""
+from typing import Any, Callable, Dict, FrozenSet, List, Optional, Tuple
+
+import numpy as np
+from sklearn import metrics
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+
+# the trend flags (趋势旗）
+TREND_FLAG_STABLE = "stable"
+"""
+代表 稳定型高频词汇
+
+定义：词汇的出现频率几乎保持不变
+"""
+
+TREND_FLAG_DECLINE = "decline"
+"""
+代表 衰退型高频词汇
+
+定义：词汇的出现频率几乎一直在递减
+"""
+
+TREND_FLAG_INCREASE = "increase"
+"""
+代表 新生型高频词汇
+
+定义：词汇的出现频率几乎一直在递增
+"""
+
+
+class StatisticalAnalyzer:
+    """
+    通过统计分析来总结此数据里的重要词汇
+
+    ！必须实现 _summarize 方法 !
+
+    Note:此类为抽象类，不应直接引用
+
+    成员变量如下：
+    === 私有变量 ===
+    _lst: 包含每个文件的高频词汇汇总
+    _whitelist_word: 返回列表里一定包含的词汇
+    _blacklist_word: 返回列表里一定不包含的词汇
+    _num_wanted: 需要的重要高频词汇的个数
+
+    === 前提条件 ===
+    - _lst 里面词汇数量 >= _num_wanted
+    - _num_wanted >= len(_whitelist_word)
+    - _whitelist_word 与 _blacklist_word 交集为空集
+    """
+    _lst: List[Dict[str, int]]
+    _whitelist_word: List[str]
+    _blacklist_word: List[str]
+
+    def __init__(self,
+                 lst: List[Dict[str, int]],
+                 whitelist_word: List[str],
+                 blacklist_word: List[str]) -> None:
+        """初始化 StatisticalAnalyzer"""
+        self._lst = lst
+        self._whitelist_word = whitelist_word
+        self._blacklist_word = blacklist_word
+
+    def _words(self) -> FrozenSet[str]:
+        """返回所有的词汇"""
+        return frozenset(word for year in self._lst for word, _ in year.items())
+
+    def _values(self, words: FrozenSet[str] = None) -> \
+            Dict[str, List[int]]:
+        """
+        返回所有词汇的对应年份的出现频率
+
+        返回参数如下：
+        :return: 一个字典，键为词汇，值为 所有年份出现频率
+        """
+        return {word: [year.get(word, 0) for year in self._lst] for word in
+                (words if words is not None else self._words())}
+
+    def _frequency(self, values: Dict[str, List[int]] = None) -> \
+            Dict[str, int]:
+        """
+        返回一个字典，键为 _lst 的词汇，值为出现次数
+
+        Note:本方法为辅助方法
+
+        返回参数如下：
+        :return: 一个字典包含所有词汇
+        """
+        return {
+            word: sum(value) for word, value in
+            (values if values is not None else self._values()).items()
+        }
+
+    def _offsets(self, values: Dict[str, List[int]] = None) -> \
+            Dict[str, List[int]]:
+        """
+        返回所有单词的 offset 值
+
+        返回参数如下:
+        :return: 一个字典，键为词汇，值为 offset 值
+        """
+        return {
+            word: self._get_offset(value) for word, value in
+            (values if values is not None else self._values()).items()
+        }
+
+    @staticmethod
+    def _get_offset(value: List[int]) -> List[int]:
+        """
+        返回 value 的 offset 值
+        """
+        offsets = []
+        last = None
+        for this in value:
+            if last is not None:
+                offsets.append(this - last)
+            last = this
+        return offsets
+
+    def _rank(self, scores: Dict[str, Any],
+              values: Dict[str, List[int]] = None,
+              key: Callable = lambda a: a) -> \
+            List[Tuple[str, List[int], Any]]:
+        """
+        返回一个排序过的列表，排序规则的 key 返回的值，与 List.sort 的 key 参数相同。
+
+        该列表为 _whitelist_word 的超集，与 _blacklist_word 的交集为空
+
+        Note:本方法为辅助方法
+
+        参数列表如下：
+        :param scores: 一个列表，键为词汇，值为词汇的衡量值，scores 一定包含所有 _whitelist_word
+        :param values: 所有词汇的每年出现频率，若不提供，则重新计算
+
+        返回参数如下：
+        :return: 一个由高到低排过序的列表
+        """
+        if values is None:
+            values = self._values()
+        # rank and filter the blacklist
+        return sorted(
+            ((word, values[word], score) for word, score in scores.items() if
+             word not in self._blacklist_word), key=lambda a: key(a[2]),
+            reverse=True
+        )
+
+    def analyse(self, *args, **kwargs) -> List[Tuple[str, List[int], Any]]:
+        """
+        返回一个排序过的列表，报告词汇分析结果，该列表包括：
+            [(词汇，词汇每年出现频率，衡量标准), ... ]
+
+        返回参数如下：
+        :return: -个字典报告重要高频词汇
+        """
+        raise NotImplementedError
+
+    @classmethod
+    def const(cls,
+              lst: List[Dict[str, int]],
+              whitelist_word: List[str],
+              blacklist_word: List[str]):
+        """创建一个 StatisticalAnalyzer object"""
+        return cls(lst, whitelist_word, blacklist_word)
+
+
+class FrequencyAnalyzer(StatisticalAnalyzer):
+    """
+    使用 出现频率 作为衡量条件的分析器
+
+    成员变量如下：
+    === 私有变量 ===
+    _lst: 包含每个文件的高频词汇汇总
+    _whitelist_word: 返回列表里一定包含的词汇
+    _blacklist_word: 返回列表里一定不包含的词汇
+
+    === 前提条件 ===
+    - _whitelist_word 与 _blacklist_word 交集为空集
+    """
+    _lst: List[Dict[str, int]]
+    _whitelist_word: List[str]
+    _blacklist_word: List[str]
+    _num_wanted: Optional[int]
+
+    def analyse(self, *args, **kwargs) -> List[Tuple[str, List[int], Any]]:
+        return self._rank(self._frequency())
+
+
+class TrendAnalyzer(StatisticalAnalyzer):
+    """
+    使用 变化趋势 作为衡量条件的分析器
+
+    成员变量如下：
+    === 私有变量 ===
+    _lst: 包含每个文件的高频词汇汇总
+    _whitelist_word: 返回列表里一定包含的词汇
+    _blacklist_word: 返回列表里一定不包含的词汇
+
+    === 前提条件 ===
+    - _whitelist_word 与 _blacklist_word 交集为空集
+    - _lst 长度至少为 2
+    - 0 <= _tolerance <= 100
+    """
+    _lst: List[Dict[str, int]]
+    _whitelist_word: List[str]
+    _blacklist_word: List[str]
+    _threshold: Tuple[float, float]
+
+    def __init__(self, lst: List[Dict[str, int]],
+                 whitelist_word: List[str],
+                 blacklist_word: List[str]) -> None:
+        super().__init__(lst, whitelist_word, blacklist_word)
+        self._threshold = (-0.4, 0.4)
+
+    def analyse(self, *args, **kwargs) -> List[Tuple[str, List[int], Any]]:
+        """
+        返回一个集 包括 _num_wanted 个在 _lst 的重要高频词汇
+
+        算法简介：
+         根据 linear regression 的 coefficient 给以下词汇分类
+            稳定型高频词汇:
+                - TREND_FLAG_STABLE
+                - 定义：词汇的出现频率几乎保持不变
+                - -0.2 < coefficient < 0.2
+            衰退型高频词汇
+                - TREND_FLAG_DECLINE
+                - 定义：词汇的出现频率几乎一直在递减
+                - coefficient >= 0.2
+            新生型高频词汇
+                - TREND_FLAG_INCREASE
+                - 定义：词汇的出现频率几乎一直在递增
+                - coefficient <= -0.2
+        """
+        values = self._values()
+        trends_score = {}
+        for word, value in values.items():
+            # we build the module
+            x = np.array(range(len(value))).reshape(-1, 1)
+            y = np.array(value).reshape(-1, 1)
+            x_train, x_test, y_train, y_test = train_test_split(
+                x, y, test_size=0.2, random_state=0
+            )
+            reg = LinearRegression()
+            reg.fit(x_train, y_train)
+            if reg.coef_ == 0:
+                continue
+            y_pred = reg.predict(x_test)
+            RMSE = np.sqrt(metrics.mean_squared_error(y_test, y_pred))
+            label = self._label(reg.coef_)
+            score_dict = {
+                "coeff": reg.coef_,  # the slop
+                "inter": reg.intercept_,  # the interception
+                "RMSE": RMSE,  # how good this module is
+                "label": label  # the label of this word
+            }
+            trends_score[word] = score_dict
+        return self._rank(
+            trends_score, values=values, key=lambda a: a["coeff"]
+        )
+
+    def _label(self, coeff: np.array) -> str:
+        """给此斜率标签"""
+        if coeff <= self._threshold[0]:
+            return TREND_FLAG_DECLINE
+        elif coeff >= self._threshold[1]:
+            return TREND_FLAG_INCREASE
+        else:
+            return TREND_FLAG_STABLE
