b6e665061f29a8730dd96697c2ebcc1c45f937d8
homelet <homeletwei@gmail.com>
2020-05-28 12:45:14 -0400

    Update Release v0.2.1

--- install_script.sh
+++ install_script.sh
@@ -3,25 +3,57 @@
 SCRIPT_DIR=$(dirname "$0")
 
 cd "$SCRIPT_DIR" || {
-  echo "Failure"
+  echo "Fail to cd to current dir"
   exit 1
 }
 
-echo "Working in $(pwd)"
+echo "工作地址： $(pwd)"
 
-echo "Installing Virtuelenv..."
+# first install Homebrew
+
+echo "正在安装 Homebrew..."
+
+/usr/bin/ruby -e "$(curl -fsSL https://cdn.jsdelivr.net/gh/ineo6/homebrew-install/install)"
+
+brew install git
+
+echo "正在配置 Homebrew 使用 清华源..."
+
+git -C "$(brew --repo)" remote set-url origin https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/brew.git
+
+git -C "$(brew --repo homebrew/core)" remote set-url origin https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/homebrew-core.git
+
+git -C "$(brew --repo homebrew/cask)" remote set-url origin https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/homebrew-cask.git
+
+git -C "$(brew --repo homebrew/cask-fonts)" remote set-url origin https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/homebrew-cask-fonts.git
+
+git -C "$(brew --repo homebrew/cask-drivers)" remote set-url origin https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/homebrew-cask-drivers.git
+
+brew update
+
+echo "正在安装 poppler..."
+
+brew install poppler
+
+echo "正在安装 tesseract..."
+
+brew install tesseract
+
+echo "正在安装 Virtuelenv..."
+
+echo "Using Mirror : https://pypi.tuna.tsinghua.edu.cn/simple"
 
 # first install virtualenv
 
-python3 -m pip install virtualenv
+python3 -m pip install -i https://pypi.tuna.tsinghua.edu.cn/simple virtualenv
 
-echo "Creating Virtuelenv enviroment..."
+echo "正在配置 Virtuelenv 环境..."
 
 virtualenv hfwa_env
 
 source hfwa_env/bin/activate
 
-echo "Installing Dependencies..."
+echo "正在安装项目依赖..."
 
 echo "Using Mirror : https://pypi.tuna.tsinghua.edu.cn/simple"
 
@@ -29,7 +61,7 @@ echo "Using Mirror : https://pypi.tuna.tsinghua.edu.cn/simple"
 
 hfwa_env/bin/python -m pip install -i https://pypi.tuna.tsinghua.edu.cn/simple -r requirements.txt
 
-echo "Prepare to build App..."
+echo "开始编译程序..."
 
 # after install complete we build the app
 
